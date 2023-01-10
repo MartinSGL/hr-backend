fb73a7a2e6630d900655d1bb4865cb1e44d84b0e
Carlos Diaz <carlosdiaz.dev@gmail.com>
2023-01-10 16:04:44 -0600

    feat: :sparkles: add entity of holiday section

--- src/holidays/entities/holiday.entity.ts
+++ src/holidays/entities/holiday.entity.ts
@@ -1,1 +1,15 @@
-export class Holiday {}
+import { Schema, Prop } from '@nestjs/mongoose';
+@Schema({ timestamps: true })
+export class Holiday {
+  @Prop({ required: true, unique: true })
+  name: string;
+
+  @Prop({ required: true, unique: true })
+  date: Date;
+
+  @Prop({ default: 'no image' })
+  image: string;
+
+  @Prop({ required: true })
+  id_tm: number;
+}
