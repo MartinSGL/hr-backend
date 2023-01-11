7e2d5c43bd7e611bda2a6d0db9dfacf16f0f1a1c
IrasemaSJ <Irasema.Serrano@improving.com>
2023-01-11 12:55:03 -0600

    feat: :sparkles: add entity to vacations module

--- src/vacations/entities/vacation.entity.ts
+++ src/vacations/entities/vacation.entity.ts
@@ -1,1 +1,32 @@
-export class Vacation {}
+import { Prop, Schema } from '@nestjs/mongoose';
+import { status, Status } from 'src/common/interfaces/status.interface';
+
+@Schema({ timestamps: true })
+export class Vacation {
+  @Prop({ required: true, unique: true })
+  folio: string;
+
+  @Prop({ required: true })
+  id_employee: number;
+
+  @Prop({ required: true })
+  init_date: Date;
+
+  @Prop({ required: true })
+  final_date: Date;
+
+  @Prop([Date])
+  half_date?: Date[];
+
+  @Prop({ required: true })
+  client_evidence: string;
+
+  @Prop({ default: 'pending', enum: status, index: true })
+  status?: Status;
+
+  @Prop({ default: '' })
+  comments?: string;
+
+  @Prop({ required: true })
+  id_tm: number;
+}
