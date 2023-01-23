eca0d93b63055f3c83d1ef729b0e73f752096d02
Martin Salvador Gaytan Lugo <martin.gaytan@improving.com>
2023-01-23 15:48:33 -0600

    feat: :sparkles: make the seed for users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,29 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseModule, Schema } from '@nestjs/mongoose';
 import { ContingenciesModule } from './contingencies/contingencies.module';
 import { HolidaysModule } from './holidays/holidays.module';
 import { VacationsModule } from './vacations/vacations.module';
+import { SeedModule } from './seeds/seeds.module';
+import { UsersModule } from './users/users.module';
+import { EnvConfiguration } from './config/app.config';
+import { JoiValidationSchema } from './config/joi.validation';
 
 @Module({
   imports: [
     //allow to read enviroment variables from .env file
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      load: [EnvConfiguration],
+      validationSchema: JoiValidationSchema,
+    }),
     //conection to mongo db
     MongooseModule.forRoot(process.env.MONGO_DB),
     // Application Mudules
     ContingenciesModule,
     HolidaysModule,
     VacationsModule,
+    SeedModule,
+    UsersModule,
   ],
 })
 export class AppModule {}
