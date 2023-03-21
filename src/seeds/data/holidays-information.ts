e53ea509091263c3322d9954954cbcde623d2d89
IrasemaSJ <Irasema.Serrano@improving.com>
2023-03-21 11:58:26 -0600

    fix: :bug: fix grammar mistake

--- src/seeds/data/holidays-information.ts
+++ src/seeds/data/holidays-information.ts
@@ -4,7 +4,7 @@ export const holidays_information: CatalogueHoliday[] = [
   { name: 'NEW YEAR', id_tm: 0 },
   { name: 'MEXICAN CONSTITUTION DAY', id_tm: 0 },
   { name: 'BENITO JUAREZ DAY', id_tm: 0 },
-  { name: 'HOLI FRIDAY', id_tm: 0 },
+  { name: 'HOLY FRIDAY', id_tm: 0 },
   { name: 'LABOR DAY', id_tm: 0 },
   { name: 'MEXICAN REVOLUTION DAY', id_tm: 0 },
   { name: 'MEXICAN INDEPENDENCE DAY', id_tm: 0 },
